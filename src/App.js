1afab5389a9ba4dec44f832c668d80e2de5f0b30
iamasianman <kevinle1991@gmail.com>
2020-07-01 23:24:52 -0700

    set up game board and hard coded the snake and the food

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Snake from './Snake';
+import Food from './Food';
 
-function App() {
-  return (
-    <div >
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    food: [6, 8],
+    snakeDots: [
+      [0, 0],
+      [2, 0]
+    ]
+  }
+  render() {
+    const { snakeDots, food } = this.state
+    return (
+      <div className='game-area'>
+        <Snake snakeDots={snakeDots}></Snake>
+        <Food dot={food}></Food>
+      </div>
+    );
+  }
 }
 
 export default App;
